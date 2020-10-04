186bd49eec0e00e5deeb691023058a2b783df0b0
crucialjun <nich.otieno@gmail.com>
2020-10-04 13:42:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cityguideapp/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.cityguideapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+    }
+}
