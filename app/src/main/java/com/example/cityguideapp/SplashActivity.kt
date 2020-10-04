72ea59f39e0c22b97a4b7cd15a413d94e760b61d
Crucialjun <nich.otieno@gmail.com>
2020-10-04 14:12:19 +0300

    Splash screen done

--- app/src/main/java/com/example/cityguideapp/SplashActivity.kt
+++ app/src/main/java/com/example/cityguideapp/SplashActivity.kt
@@ -1,14 +1,35 @@
 package com.example.cityguideapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIMER : Long = 5000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
+
+        val sideAnim = AnimationUtils.loadAnimation(this,R.anim.side_animation)
+        val bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_anim)
+
+        background_image.animation = sideAnim
+        powered_by_line.animation = bottomAnim
+
+        Handler().postDelayed(object : Runnable{
+            override fun run() {
+                val intent = Intent(applicationContext,UserDashBoard::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        },SPLASH_TIMER)
     }
 }
---
+++ app/src/main/java/com/example/cityguideapp/UserDashBoard.kt
@@ -0,0 +1,11 @@
+package com.example.cityguideapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserDashBoard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_dash_board)
+    }
+}
