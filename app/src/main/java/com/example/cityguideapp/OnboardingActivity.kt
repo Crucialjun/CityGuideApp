a51aac8d7916d73e3dd183c82e5d15dcb9c44b9b
Crucialjun <nich.otieno@gmail.com>
2020-10-05 12:01:26 +0300

    Change Get started button visibility

--- app/src/main/java/com/example/cityguideapp/OnboardingActivity.kt
+++ app/src/main/java/com/example/cityguideapp/OnboardingActivity.kt
@@ -1,14 +1,30 @@
 package com.example.cityguideapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import kotlinx.android.synthetic.main.activity_onboarding.*
+
+private val ViewPager.animation1: Animation?
+    get() {
+        val animation = AnimationUtils.loadAnimation(context, R.anim.bottom_anim)
+        return animation
+    }
 
 class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_onboarding)
 
 
@@ -16,24 +32,63 @@ class OnboardingActivity : AppCompatActivity() {
 
         vp_on_boarding.apply {
             adapter = SliderAdapter(context)
+            addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+                override fun onPageScrolled(
+                    position: Int,
+                    positionOffset: Float,
+                    positionOffsetPixels: Int
+                ) {
+
+                }
+
+                override fun onPageSelected(position: Int) {
+                    addDots(position)
+
+                    when (position) {
+                        0 -> btn_get_started.visibility = View.INVISIBLE
+                        1 -> btn_get_started.visibility = View.INVISIBLE
+                        2 -> btn_get_started.visibility = View.INVISIBLE
+                        else -> {
+                            val animation =
+                                AnimationUtils.loadAnimation(context, R.anim.bottom_anim)
+                            btn_get_started.animation = animation
+                            btn_get_started.visibility = View.VISIBLE
+
+                        }
+
+                    }
+                }
+
+                override fun onPageScrollStateChanged(state: Int) {
+
+                }
+
+            })
         }
 
-        addDots()
+        addDots(0)
 
 
     }
 
-    fun addDots(){
-        val dots = arrayOfNulls<TextView>(4,)
+    private fun addDots(position: Int) {
+        val dots = arrayOfNulls<TextView>(4)
+        dots_layout.removeAllViews()
 
-        for (i in dots.indices){
+        for (i in dots.indices) {
             dots[i] = TextView(this)
             dots[i]?.text = Html.fromHtml("&#8226;")
-            dots[i]?.textSize= 35F
+            dots[i]?.textSize = 35F
 
             dots_layout.addView(dots[i])
         }
 
+        if (dots.isNotEmpty()) {
+            dots[position]?.setTextColor(resources.getColor(R.color.colorPrimaryDark))
+        }
+
 
     }
+
+
 }
